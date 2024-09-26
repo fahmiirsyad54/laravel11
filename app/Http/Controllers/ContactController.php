21c2355c3a32f79b58f91e86597994a8ed089c6b
unknown <fahmiirsyad54@gmail.com>
2024-09-26 11:22:40 +0700

    reuse component and setup navigation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -12,7 +12,7 @@ class ContactController extends Controller
     public function index()
     {
         return view('contact', [
-            'title'     => 'contact',
+            'title'     => 'Contact',
             'nama'      => 'Nama kalian',
             'kelas'     => 'Kelas kalian',
             'linkedin'  => 'alamat url linkedin',
